06a18b777d48efd25efd70635c84b0837f7e104c
Wolfspirit1997 <camilo_19041997@hotmail.es>
2022-07-27 20:26:39 -0500

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,15 +1,15 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = null; 
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 'hola';
 
 // Crea una variable booleana:
 const nuevoBool = null;
 
-// Resuelve el siguiente problema matemático:
+//        Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
 
 // Resuelve el siguiente problema matemático:
